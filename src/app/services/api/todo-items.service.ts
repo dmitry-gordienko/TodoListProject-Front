57352c00fe199e0144ee3c8f4fbc0e432fd417c3
Dmitry Gordienko <dmitry.gordienko.itp@gmail.com>
2021-06-03 15:44:04 +0300

    A new method in the TodoItemsService - GetItemsByListId()

--- src/app/services/api/todo-items.service.ts
+++ src/app/services/api/todo-items.service.ts
@@ -1,4 +1,8 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthorizationService } from '../../services/common/authorization.service';
+import { ConfigurationService } from '../../services/common/configuration.service';
 
 export interface ITodoItem {
   id: number;
@@ -7,10 +11,42 @@ export interface ITodoItem {
   isDone: boolean;
 }
 
+export interface IAddItemRequest {
+  name: string;
+  todoListId: number;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class TodoItemsService {
 
-  constructor() { }
+  constructor(
+    private authorizationService: AuthorizationService,
+    private config: ConfigurationService,
+    private httpClient: HttpClient
+  ) { }
+
+
+  GetItemsByListId(listId: number): Observable<ITodoItem[]>
+  {
+    const accessToken = localStorage.getItem(this.config.accessTokenName);
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${accessToken}`);
+    
+    const url = this.config.apiTodoItemsUrl + '?todoListId=' + listId;
+
+    return this.httpClient.get<ITodoItem[]>(url, { 'headers': headers });
+  }
+  
+  AddNewItemToList(itemName:string, listId: number): Observable<ITodoItem>
+  {
+    const accessToken = localStorage.getItem(this.config.accessTokenName);
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${accessToken}`);
+    
+    const body = new FormData();
+    body.append('name', itemName);
+    body.append('todoListId', listId.toString());
+    return this.httpClient.post<ITodoItem>(this.config.apiTodoItemsUrl, body, {'headers': headers});
+  }
+  
 }
